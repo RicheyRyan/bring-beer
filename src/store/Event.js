a88f18a08389c36dc9f0b9c0a46ea3d37b7ef2ad
Richey Ryan <richeyryan@protonmail.com>
2021-02-28 21:49:34 +0000

    Refactor event loading

--- src/store/Event.js
+++ src/store/Event.js
@@ -15,26 +15,20 @@ const eventsColl = db.collection("events");
 
 export const createNewEvent = createEffect(eventsColl.doc().set);
 
+const mapEvents = (action) => (snapshot) => {
+  const events = snapshot.docs.map((doc) => ({
+    id: doc.id,
+    ...doc.data(),
+  }));
+  action(events);
+};
+
 eventsColl
   .where("scheduled", ">", Date.now())
   .orderBy("scheduled", "asc")
-  .onSnapshot((snapshot) => {
-    const events = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    loadEvents(events);
-  });
+  .onSnapshot(mapEvents(loadEvents));
 eventsColl
   .where("scheduled", "<", Date.now())
   .orderBy("scheduled", "desc")
   .limit(5)
-  .onSnapshot((snapshot) => {
-    const events = snapshot.docs.map((doc) => ({
-      id: doc.id,
-      ...doc.data(),
-    }));
-    loadPastEvents(events);
-  });
-
-allEvents.watch(console.log);
+  .onSnapshot(mapEvents(loadPastEvents));
